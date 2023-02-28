b1cae0a00ed8e19b7817e9358b63f196ad11f3b9
YiLunLee <41618081+YiLunLee@users.noreply.github.com>
2023-02-28 16:51:49 +0800

    tmp_v1

--- README.md
+++
@@ -1,1 +0,0 @@
-# Multimodal Prompting with Missing Modalities for Visual Recognition (CVPR 2023)
---
+++ static/asset/mystyle.js
@@ -0,0 +1,39 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  //slides[slideIndex-1].style.display = "block"; 
+  //dots[slideIndex-1].className += " active";
+}
+
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleTopNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
